74221e9e9eb23e7a46cebc7ee6fc1461462e8d41
雪中听琴 <git@hirrao.com>
2023-12-13 01:35:13 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+    width: 800,
+    height: 600,
+  });
+  mainWindow.setMenu(null);
+  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
